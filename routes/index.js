3dab5d88aa4e3dbe8fd3eb98908c639c356e9bd9
Sariah <sariahwandler@gmail.com>
2025-02-19 12:07:40 -0700

    jwt debug

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 const router = require('express').Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-
 const JWT_SECRET = 'your_jwt_secret_key';
 
 router.use('/', require('./swagger'));
-
 router.use('/users', require('./users'));
-
 router.use('/events', require('./events'));
 
 const generateToken = (user) => {
@@ -15,24 +12,30 @@ const generateToken = (user) => {
 };
 
 router.post('/login', (req, res, next) => {
-    console.log("Login route hit with data:", req.body); // Add logging to confirm
+    console.log("Login route hit with data:", req.body);
     next();
 }, passport.authenticate('local', { failureFlash: false }), (req, res) => {
     const token = generateToken(req.user);
-    console.log("User after login:", req.user); // Logging user object
+    console.log("User after login:", req.user);
 
     const responseData = {
         userId: req.user._id.toString(),
         username: req.user.username,
         favorites: req.user.favorites,
         dislikes: req.user.dislikes,
-        token: token // Include the JWT token in the response
+        token: token
     };
     res.json({ message: 'Logged in successfully', user: responseData });
 });
 
-router.get('/logout', function (req, res, next) {
-    req.logout(function (err) {
+// Add error handling middleware
+router.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'An internal server error occurred' });
+});
+
+router.get('/logout', (req, res, next) => {
+    req.logout((err) => {
         if (err) {
             return next(err);
         }

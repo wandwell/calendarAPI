78b6feee1b972af30b0c101687155f172709d926
Sariah <sariahwandler@gmail.com>
2025-02-18 17:24:30 -0700

    fix header

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app
     .use(passport.session())
 
     .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Update to your frontend URL
+        res.setHeader('Access-Control-Allow-Origin', 'https://ontheplate.netlify.app/'); // Update to your frontend URL
         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         res.setHeader('Access-Control-Allow-Credentials', 'true');

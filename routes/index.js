2d2fca03c96286321fec8fcc1b082d5271265e63
Sariah <sariahwandler@gmail.com>
2025-02-19 08:54:56 -0700

    fix login route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-const passport = require('passport');
 const router = require('express').Router();
+const passport = require('passport');
 
 router.use('/', require('./swagger'));
 
@@ -7,34 +7,24 @@ router.use('/users', require('./users'));
 
 router.use('/events', require('./events'));
 
-router.post('/login', (req, res, next) => {
-    //#swaggertags=['login']
-    const { username, password } = req.body; // Accepting username and password from the request body
-    passport.authenticate('local', (err, user, info) => {
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-            return res.status(401).json({ message: 'Invalid username or password' }); // Provide a clear response for failure
-        }
-        req.login(user, (err) => {
-            if (err) {
-                return next(err);
-            }
-            req.session.user = user;
+app.post('/login', (req, res, next) => {
+    console.log("Login route hit with data:", req.body); // Add logging to confirm
+    next();
+}, passport.authenticate('local', { failureFlash: false }), (req, res) => {
+    req.session.user = req.user;
+    console.log("User after login:", req.user); // Logging user object
+    console.log("Session after login:", req.session); // Logging session
 
-            // Prepare JSON response
-            const responseData = {
-                userId: user._id.toString(),
-                username: user.username,
-                favorites: user.favorites,
-                dislikes: user.dislikes
-            };
+    const responseData = {
+        userId: req.user._id.toString(),
+        username: req.user.username,
+        favorites: req.user.favorites,
+        dislikes: req.user.dislikes
+    };
 
-            return res.json({ message: 'Logged in successfully', user: responseData }); // Return JSON response instead of redirecting
-        });
-    })(req, res, next);
+    res.json({ message: 'Logged in successfully', user: responseData });
 });
+
 
 router.get('/logout', function (req, res, next) {
     req.logout(function (err) {

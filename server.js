696ec632a0453c8166ff21d303da76fa410ef82a
Sariah <sariahwandler@gmail.com>
2025-02-19 04:01:47 -0700

    fix cors options

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.port || 3000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // Update to your frontend URL
+    origin: 'https://ontheplate.netlify.app/', // Update to your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true, // Allow credentials
 }

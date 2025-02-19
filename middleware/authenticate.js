bbe94e0254685e3f5ba85f94995b7fec66ae1822
Sariah Wandler <120832656+wandwell@users.noreply.github.com>
2025-02-19 09:18:35 -0700

    Update authenticate.js

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,8 +1,10 @@
 const { User } = require('../models/users');
 
 const isAuthenticated = (req, res, next) => {
+    console.log("Session ID in isAuthenticated middleware:", req.sessionID); // Log session ID
     console.log("Session in isAuthenticated middleware:", req.session);
-    if (req.session.user === undefined) {
+    if (!req.session.user) {
+        console.error("User not found in session");
         return res.status(401).json('You do not have access');
     }
     next();
